889a466d9774e7bcce9c5016d1d96628e7959c73
jmuders-gcs <jmuders-gcs@users.noreply.github.com>
2026-06-15 00:09:54 +0000

    deploy: a497a1ea03e719a82b0497106ddb065502bf537e

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781481434|3922269';
+const CACHE_VERSION = '1781482191|4156067';
 /** @type {string} */
 const CACHE_PREFIX = 'Drag Race-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
